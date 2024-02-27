33030717ccdb7f3ab75ab9289babb9b338c0ea65
jaycelmarie <C20372876@mytudublin.ie>
2024-02-27 22:34:22 +0000

    api.py, api2.py and app.py changes

--- api2.py
+++ api2.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import torch
 import torchvision.transforms.functional as tvf
 
-from utils import visualization, dataloader, utils
+from utils import visualization, utils
 
 import cv2
 import random
@@ -24,7 +24,7 @@ class Detector():
         conf_thres: float, confidence threshold
         input_size: int, input resolution
     '''
-    def __init__(self, model_name='', weights_path=None, model=None, **kwargs):
+    def __init__(self, model_name='rapid', weights_path=None, model=None, **kwargs):
         # post-processing settings
         self.conf_thres = kwargs.get('conf_thres', None)
         self.input_size = kwargs.get('input_size', None)
@@ -35,16 +35,25 @@ class Detector():
         if model_name == 'rapid':
             from models.rapid import RAPiD
             model = RAPiD(backbone='dark53')
-        elif model_name == 'rapid_export': # testing-only version
-            from models.rapid_export import RAPiD
-            model = RAPiD()
         else:
             raise NotImplementedError()
         total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
         print(f'Successfully initialized model {model_name}.',
             'Total number of trainable parameters:', total_params)
+        
+        # Name changes for backwards compatibility
+        weights_dict = torch.load(weights_path, map_location='cpu')
+        assert 'model_state_dict' in weights_dict or 'model' in weights_dict
+        if 'model_state_dict' in weights_dict:
+            model_dict = weights_dict['model_state_dict']
+        elif 'model' in weights_dict:
+            model_dict = weights_dict['model']
+        for name in list(model_dict.keys()):
+            if name.startswith("module"):
+                model_dict[name[7:]] = model_dict[name]
+                del model_dict[name]
 
-        model.load_state_dict(torch.load(weights_path, map_location='cpu')['model'])
+        model.load_state_dict(model_dict, strict=False)
         print(f'Successfully loaded weights: {weights_path}')
         model.eval()
         if kwargs.get('use_cuda', True):
@@ -86,9 +95,9 @@ class Detector():
             save_path = './uploads'
        	    cv2.imwrite(os.path.join(save_path,'savedimg' + '_' + str(random.randint(1,1000)) + '.jpg'), img_rgb)
             print('==== Image has been saved locally! ====')
-       
+            
         return detections
-
+        
     def detect_imgSeq(self, img_dir, **kwargs):
         '''
         Run on a sequence of images in a folder.
@@ -118,7 +127,7 @@ class Detector():
                 detection_json.append(dt_dict)
 
         return detection_json
-
+    
     def _predict_pil(self, pil_img, **kwargs):
         '''
         Args:
@@ -164,7 +173,6 @@ class Detector():
             plt.show()
         return dts
 
-
 def detect_once(model, pil_img, conf_thres, nms_thres=0.45, input_size=608):
     '''
     Run the model on the pil_img and return the detections.
@@ -179,8 +187,4 @@ def detect_once(model, pil_img, conf_thres, nms_thres=0.45, input_size=608):
     dts = dts[dts[:,5] >= conf_thres].cpu()
     dts = utils.nms(dts, is_degree=True, nms_thres=0.45)
     dts = utils.detection2original(dts, pad_info.squeeze())
-    # np_img = np.array(pil_img)
-    # api_utils.draw_dt_on_np(np_img, detections)
-    # plt.imshow(np_img)
-    # plt.show()
     return dts
