4b3066c8cadf5c043d779c53a80761e21ac11c91
Jaycel Estrellado <98519686+jaycelmarie@users.noreply.github.com>
2024-03-12 19:36:38 +0000

    capture cam python created

--- capturecam.py
+++ capturecam.py
@@ -1,18 +1,20 @@
 import cv2
 import time
 import os
+from apicam import Detector
+import numpy as np
 
 # Set countdown timer
-timer = int(5)
+timer = int(20)
 
-source = ----
+source = "rtsp://user:test1234@192.168.1.101:8554/profile0"
 cam = cv2.VideoCapture(source)
 
 while True:
     check, frame = cam.read()
 
     prev = time.time()
-
+  
     while timer >= 0:
 
         check, frame = cam.read()
@@ -38,16 +40,27 @@ while True:
 
     else:
         # Save the frame 
-        file_path = os.path.join(save_folder, 'camera.jpg')
+        file_path = os.path.join('uploads', 'camera.jpg')
         cv2.imwrite(file_path, frame) 
 
         print("=== Photo taken! ===")
-        timer = 5
+        timer = 20
+        
+        # Initialize detector
+        detector = Detector(model_name='rapid',
+                   weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt',
+                   use_cuda=False)
 
-    # Wait for a short delay (10 milliseconds) to ensure the frame updates
-    if cv2.waitKey(10) == ord("q"):
+	# A simple example to run on a single image and plt.imshow() it
+        detector.detect_one(img_path='./uploads/camera.jpg',
+                            input_size=1024, conf_thres=0.3,
+                            visualize=True)
+		
+   # Wait for a short delay (10 milliseconds) to ensure the frame updates
+    if cv2.waitKey(20) == ord("q"):
+       print("Exiting..")
        break
-
-
+       
 cam.release()
 cv2.destroyAllWindows()
+
